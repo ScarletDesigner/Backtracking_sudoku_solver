233a27115f4d4beb0170415c11a442bc4aa0c469
ScarletDesigner <50834371+ScarletDesigner@users.noreply.github.com>
2019-05-28 14:49:34 +0200

    Delete Backtracking_sudoku_solver.h

--- Backtracking_sudoku_solver.h
+++
@@ -1,9 +0,0 @@
-bool solveSudoku(int, int);
-void loadFile();
-void printGameboard();
-bool isEnded(int);
-bool foundPossibleNumberToPutInCell(int, int);
-bool isPossibleToPutInCell(int, int, int);
-bool rowHasThatNumber(int, int, int);
-bool columnHasThatNumber(int, int, int);
-bool subsquareHasThatNumber(int, int, int);
